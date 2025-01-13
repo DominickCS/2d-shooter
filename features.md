136d5fd2b77ad9d09acbec0f07ea7d1b8c2d7a52
Dominick Smith <dominick.smith938@gmail.com>
2025-01-13 11:46:38 -0600

    inital commit

---
+++ features.md
@@ -0,0 +1,11 @@
+The features of the shooter zombies style game consist of a stationary player in the middle of the map, able to fire projectiles, laser like and hit enemies to explode/kill them
+
+pickup powerups by shooting them, collateral damage, nuke, double xp, etc
+
+different enemy types, normal enemy - one shot, mini bosses / more hits to kill, higher score, guaranteed drop
+
+faster enemy speed as time progresses
+
+limited ammo, have to either wait (max projectiles on screen at once) or reload ammo count should be relatively generous (create ammo instant reload power up if implemented)
+
+should feature a scoreboard and start menu
---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame
+import random
+import numpy as np
+import math
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((900, 600))
+pygame.display.set_caption("zombies")
+clock = pygame.time.Clock()
+
+
+# def angle_between_vectors_np(u, v):
+#     u = np.array(u)
+#     v = np.array(v)
+#     cos_theta = np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+#     angle_rad = np.arccos(np.clip(cos_theta, -1.0, 1.0))
+#     angle_deg = np.degrees(angle_rad)
+#     return angle_rad, angle_deg
+
+def calculate_angle(p1, p2) -> float:
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    angle = math.atan2(dy, dx)  # radians
+    angle = math.degrees(angle)  # convert to degrees
+    return angle + 360 if angle < 0 else angle
+
+A = (450, 300)
+
+class Turret(pygame.sprite.Sprite):
+    def __init__(self, color, height, width, x=0, y=0):
+        super().__init__()
+
+        #pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        #self.image.set_colorkey("White")
+
+        pygame.draw.rect(self.image, color, pygame.Rect(x,y, width, height))
+        self.rect = self.image.get_rect()
+
+laser_list = pygame.sprite.Group()
+player = pygame.sprite.Group()
+turretOne = Turret("White", 20, 20)
+laser = Turret("White", 10, 1)
+turretOne.rect.x = 450
+turretOne.rect.y = 300
+player.add(turretOne)
+while True:
+    B = pygame.mouse.get_pos()
+    # print(B)
+    # print(angle_between_vectors_np(A,B))
+    project_angle = print(round((calculate_angle(A,B))))
+    screen.fill("BLACK")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements, and update game aspects
+    player.draw(screen)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        #print("shooting")
+        #player_sprite_list.add(laser)
+        if len(laser_list) <= 2:
+            laser_list.add(laser)
+            print("shooting")
+            # laser.rect.y = random.randint(0, 600)
+            laser_list.draw(screen)
+            pygame.display.update()
+            #pygame.time.delay(100)
+    for laser in laser_list:
+            laser.rect.x = laser.rect.x + 5
+            if laser.rect.x >= 900:
+                laser.kill()
+                print(len(laser_list))
+    pygame.display.update()
+    clock.tick(60)
